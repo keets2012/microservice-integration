92206f2ceada0bb2ccacdfd3431ea6812e6a66c4
keets2012 <928420597@qq.com>
2018-04-03 23:00:44 +0800

    fix eureka bugs

--- auth-server/src/main/java/com/blueskykong/auth/AuthApplication.java
+++ auth-server/src/main/java/com/blueskykong/auth/AuthApplication.java
@@ -1,6 +1,5 @@
 package com.blueskykong.auth;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
